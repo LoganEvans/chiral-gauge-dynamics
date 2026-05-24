71e82202d8b5edc283a1a073cfc103f21a2c85e9
Logan Evans <loganpevans@gmail.com>
2026-05-23 17:29:52 -0700

    Remove staging files.

--- CGD/Gravity/ExactSolutionsPart25_3.lean
+++
@@ -1,4 +0,0 @@
--- FILENAME: CGD/Gravity/ExactSolutionsPart25_3.lean
-
-import CGD.Gravity.ExactSolutionsPart25_2
--- File obsoleted by macro scalarization logic in ExactSolutionsPart25_2.lean
--- CGD/Gravity/ExactSolutionsPart25_4.lean
+++
@@ -1,4 +0,0 @@
--- FILENAME: CGD/Gravity/ExactSolutionsPart25_4.lean
-
-import CGD.Gravity.ExactSolutionsPart25_3
--- File obsoleted by macro scalarization logic in ExactSolutionsPart25_2.lean
